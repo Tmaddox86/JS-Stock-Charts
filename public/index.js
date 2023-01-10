4cadbb852b317885d8182047e592ceed6f741499
Tara Maddox <TMaddox85@gmail.com>
2023-01-09 20:57:00 -0800

    tried to add myChart

--- public/index.js
+++ public/index.js
@@ -6,4 +6,25 @@ async function main() {
 
 }
 
+const { GME, MSFT, DIS, BNTX } = mockData;
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+
 main(07b0ae594b064ffc839ee15f41d4184f)
+let response = await fetch('GME','MSFT','DIS','BNTX');
+let resultObject = await response.json();
+console.log()
+
+let GME = result.GME
+let MSFT = result.MSFT
+let DIS = result.DIS
+let BTNX = result.BTNX
+
+const stocks = [GME, MSFT, DIS, BNTX];
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
