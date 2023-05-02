3b4ab970c4e72f4ddcaf79fa9a229f653486914a
Tara Maddox <TMaddox85@gmail.com>
2023-05-02 08:56:37 -0700

    moved some things around in js file

--- public/index.js
+++ public/index.js
@@ -3,25 +3,27 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+    const { BNTX } = mockData;
+    const stock = [GME, MSFT, DIS, BNTX];
+    
+    let response = await fetch('GME','MSFT','DIS','BNTX')
+        let resultObject = await response.json()
+        console.log()
 
 }
-
-const { BNTX } = mockData;
-
-const stock = [GME, MSFT, DIS, BNTX];
-
-
 main()
-let response = await fetch('GME','MSFT','DIS','BNTX');
-let resultObject = await response.json();
-console.log()
 
+
+/*const { BNTX } = mockData;
+const stock = [GME, MSFT, DIS, BNTX];
+let response = await fetch('GME','MSFT','DIS','BNTX') => {
+let resultObject = await response.json()
+console.log()
+}
 let GME = result.GME
 let MSFT = result.MSFT
 let DIS = result.DIS
-let BTNX = result.BTNX
-
-const stocks = [GME, MSFT, DIS, BNTX];
+let BTNX = result.BTNX*/
 
 // Bonus Note: 
 // Another way to write the above lines would to refactor it as:
